547132576d76fe5cb59526b466258e5d240d14d5
Aevin Bulaong <abulaong@gmail.com>
2021-10-03 17:53:59 +0800

    merged two configuration folders

--- src/main/java/com/grayberry/grayberry/configurations/GrayberryConfiguration.java
+++ src/main/java/com/grayberry/grayberry/configurations/GrayberryConfiguration.java
@@ -1,4 +1,4 @@
-package com.grayberry.grayberry.configuration;
+package com.grayberry.grayberry.configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
